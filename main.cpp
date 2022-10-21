d0f6dc13126d92306ca8f9a92b998068e7013d12
lowkey <247730002@qq.com>
2022-10-21 10:27:20 +0800

    add triangles

--- main.cpp
+++ main.cpp
@@ -134,6 +134,14 @@ int main() {
         glVertex2f(0,0);
         glVertex2f(0.5,0.5);
         glEnd();
+
+        glColor3f(0, 1, 0);
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5, 0);
+        glVertex2f(0, 0.5);
+        glVertex2f(0.5, 0);
+        glEnd();
+
         glFlush();
         // 不交换背景色不会变化， 默认黑色
         glfwSwapBuffers(windows);
